c6bdec6bfde5f39a4a63a30fdb3a0b929a72b0ec
Thorsten Enderlein <49591978+endoleg@users.noreply.github.com>
2021-05-25 13:02:57 +0200

    Update windows-updates.ps1

--- windows-updates.ps1
+++ windows-updates.ps1
@@ -1,4 +1,15 @@
-https://github.com/brandontylerstevens/BrandonTylerStevens-PowerShell/tree/Main/Windows%20Updates
+#https://github.com/brandontylerstevens/BrandonTylerStevens-PowerShell/tree/Main/Windows%20Updates
+
+Function Build-CVEReport {### Install the module from the PowerShell Gallery (must be run as Admin)
+Install-Module -Name msrcsecurityupdates -force
+Import-module msrcsecurityupdates
+Set-MSRCApiKey -ApiKey "1bd79db501ce49a5ae1a117a2de252c8" -Verbose
+
+Get-MsrcCvrfDocument -ID "$((get-date).Year)-May" | Get-MsrcSecurityBulletinHtml -Verbose | Out-File C:\Windows\Temp\MSRCSecurityUpdates.html
+}
+
+
+
 
 $computers = 'server1',"server2"
 Function Get-LastUpdateInfo
