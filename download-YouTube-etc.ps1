f5f2ab51c20e2164f671ffe7bce9725d45f3799d
Thorsten Enderlein <49591978+endoleg@users.noreply.github.com>
2021-07-25 23:08:35 +0200

    Update download-YouTube-etc.ps1

--- download-YouTube-etc.ps1
+++ download-YouTube-etc.ps1
@@ -12,3 +12,10 @@ https://en.savefrom.net/12-how-to-download-soundcloud-audio.html
 https://github.com/iv-org/documentation/blob/master/Invidious-Instances.md
 
 https://redirect.invidious.io/
+
+https://jeffkeeling.github.io/youtube_rss_extractor/
+
+Watch videos together with your family and friends! Set up a virtual room, 
+Paste the url of whatever u wanna watch, invite others, then enjoy.
+https://w2g.tv
+Share YouTube, Vimeo, Twitch, Twitter, Instagram, your screen and more…
